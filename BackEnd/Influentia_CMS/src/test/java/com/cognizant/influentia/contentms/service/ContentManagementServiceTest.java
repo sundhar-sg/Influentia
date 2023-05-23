a8ecff783949f60ee0e3f7162d9449b1893da3ce
Sundhar S G <sundhargopal462@outlook.com>
2023-05-23 19:18:51 +0530

    Angular Update

--- BackEnd/Influentia_CMS/src/test/java/com/cognizant/influentia/contentms/service/ContentManagementServiceTest.java
+++ BackEnd/Influentia_CMS/src/test/java/com/cognizant/influentia/contentms/service/ContentManagementServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.*;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
