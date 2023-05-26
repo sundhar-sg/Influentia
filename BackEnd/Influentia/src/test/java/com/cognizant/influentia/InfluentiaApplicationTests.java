78e89bddbd0b396513ef63fdd3dd9e9dc6afa0c1
Sundhar S G <sundhargopal462@outlook.com>
2023-05-26 18:21:56 +0530

    Temp files changes

---
+++ BackEnd/Influentia/src/test/java/com/cognizant/influentia/InfluentiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.influentia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluentiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
